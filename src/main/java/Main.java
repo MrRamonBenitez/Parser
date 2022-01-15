cb786d6f1d75f0cc28e079c0ebb8928ecdb160e8
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-01-15 13:18:47 +0500

    Class Main modyfied

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,16 @@ import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,9 +27,9 @@ public class Main {
 
         String fileName = "data.csv";
         String[] employee1 = "1,John,Smith,USA,25".split(",");
-        makeCsv(employee1, fileName);
+        makeCSV(employee1, fileName);
         String[] employee2 = "2,Inav,Petrov,RU,23".split(",");
-        makeCsv(employee2, fileName);
+        makeCSV(employee2, fileName);
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
 
@@ -28,6 +37,28 @@ public class Main {
         String json = listToJson(list);
         fileName = "data.json";
         writeString(json, fileName);
+
+        fileName = "data.xml";
+        try {
+            makeXMLEmployee(fileName);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+
+    private static void makeCSV(String[] employee, String name) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(name, true))) {
+            writer.writeNext(employee);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static List<Employee> parseCSV(String[] colMap, String name) {
@@ -54,7 +85,7 @@ public class Main {
         return json;
     }
 
-    private static void writeString (String json, String fileName) {
+    private static void writeString(String json, String fileName) {
         try (FileWriter writer = new FileWriter(fileName, true)) {
             writer.write(String.valueOf(json));
             writer.append('\n');
@@ -64,11 +95,31 @@ public class Main {
         }
     }
 
-    private static void makeCsv(String[] employee, String name) {
-        try (CSVWriter writer = new CSVWriter(new FileWriter(name, true))) {
-           writer.writeNext(employee);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static void makeXMLEmployee(String fileName) throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+        Element staff = document.createElement("staff");
+        document.appendChild(staff);
+        Element record1 = document.createElement("employee");
+        record1.setAttribute("id", "1");
+        record1.setAttribute("firstName", "John");
+        record1.setAttribute("lastName", "Smith");
+        record1.setAttribute("country", "USA");
+        record1.setAttribute("age", "25");
+        staff.appendChild(record1);
+        Element record2 = document.createElement("employee");
+        record2.setAttribute("id", "2");
+        record2.setAttribute("firstName", "Inav");
+        record2.setAttribute("lastName", "Petrov");
+        record2.setAttribute("country", "RU");
+        record2.setAttribute("age", "23");
+        staff.appendChild(record2);
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File(fileName));
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(domSource, streamResult);
     }
+
 }
